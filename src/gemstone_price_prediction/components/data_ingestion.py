a76e932ccb1b75b1b50e650923ae77a16e43f305
JisnaP <jisna12@gmail.com>
2025-01-29 13:14:19 +0300

    code updated

---
+++ src/gemstone_price_prediction/components/data_ingestion.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from src.logger.logging import logging
+from src.gemstone_price_prediction.execption.execption import customexception
+
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+
+    def initiate_data_ingestion(self):
+        logging.info("data ingestion started")
+        try:
+            data=pd.read_csv(r"C:\Users\jisna\Downloads\raw.csv")
+            logging.info(" reading a csv file")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" i have saved the raw dataset in artifact folder")
+            
+            logging.info("here i have performed train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion part completed")
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+
+        except Exception as e:
+            logging.info()
+            raise custom_exception(e,sys)
+
+
+if __name__=="__main__":
+    dataingestion=DataIngestion()
+
+    dataingestion.initiate_data_ingestion()
